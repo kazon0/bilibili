5e7321843a7a13d84fd230045bfec46c63989a22
kazon0 <142081446+kazon0@users.noreply.github.com>
2025-07-29 14:24:35 +0800

    Initial Commit

---
+++ BiliBili/BiliBiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiliBiliApp.swift
+//  BiliBili
+//
+//  Created by 郑金坝 on 2025/7/29.
+//
+
+import SwiftUI
+
+@main
+struct BiliBiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiliBili/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiliBili
+//
+//  Created by 郑金坝 on 2025/7/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
